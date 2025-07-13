795b2d6eef4b3595ed8175bef8d0a25849a75c3c
Vladyslav Papshoia Lyulya <119086408+Vladispap@users.noreply.github.com>
2025-07-13 13:46:22 +0200

    main_controller

--- app/main_controller.py
+++ app/main_controller.py
@@ -4,19 +4,21 @@ from datetime import datetime, timedelta
 from app.utils.yolo_detect import detect_keypoints
 from app.utils.angulos import get_leaf_angles
 from app.utils.export import exportar_csv_por_planta
+from app.utils.draw_detections import draw_detections
 
 class VideoProcessingWorker(QThread):
     progress_changed = Signal(int)
     finished = Signal(str)
     cancelled = Signal()
 
-    def __init__(self, video_paths, output_dir="output", frame_skip=1, init_time="1/1/2025 12:00", min_per_frame=1):
+    def __init__(self, video_paths, output_dir="output", frame_skip=1, init_time="1/1/2025 12:00", min_per_frame=1, visualization=False):
         super().__init__()
         self.video_paths = video_paths
         self.output_dir = output_dir
         self.frame_skip = frame_skip
         self.init_time = init_time
         self.min_per_frame = min_per_frame
+        self.visualization = visualization
         self._is_running = True
 
     def run(self):
@@ -43,6 +45,16 @@ class VideoProcessingWorker(QThread):
                         break
 
                     detections = detect_keypoints(frame)
+                    
+                    if self.visualization:
+                        frame_with_detections = draw_detections(frame.copy(), detections)
+                        height, width = frame_with_detections.shape[:2]
+                        new_width = int(width * 0.5)
+                        new_height = int(height * 0.5)
+                        frame_with_detections = cv2.resize(frame_with_detections, (new_width, new_height))
+                        cv2.imshow("Detections", frame_with_detections)
+                        cv2.waitKey(1)
+
                     for plant_id, det in enumerate(detections):
                         angles = get_leaf_angles(det['keypoints'])
                         minutes_since_start = frame_id * self.min_per_frame * self.frame_skip
@@ -62,8 +74,10 @@ class VideoProcessingWorker(QThread):
 
         if self._is_running:
             exportar_csv_por_planta(data, output_dir=self.output_dir)
+            cv2.destroyAllWindows()
             self.finished.emit(self.output_dir)
         else:
+            cv2.destroyAllWindows()
             self.cancelled.emit()
 
     def estimate_total_frames(self):
